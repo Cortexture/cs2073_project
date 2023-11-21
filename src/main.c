017b9cf3f7820ed7e5e9f92b76938a6ab8947ba6
Cortexture <marioacabada@gmail.com>
2023-11-21 14:07:14 -0600

    just testing using void as argument

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+void blah1() {
+  printf("blah 1\n");
+}
+
+void blah2(void) {
+  printf("blah 2\n");
+}
+
 int main() {
+  blah1();
+  blah2();
+  blah1(2);
+  blah2(2);
   return 0;
 }
