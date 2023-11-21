fe48aeddd82ac4e230dd66a6f3c93090abe67439
Cortexture <marioacabada@gmail.com>
2023-11-21 14:12:10 -0600

    removed experiment

--- src/main.c
+++ src/main.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 
-void blah1() {
-  printf("blah 1\n");
-}
-
-void blah2(void) {
-  printf("blah 2\n");
-}
-
 int main() {
-  blah1();
-  blah2();
-  blah1(2);
-  blah2(2);
   return 0;
 }
