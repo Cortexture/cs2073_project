912465a0132c0a836a598d491f14d83a88a2792d
Cortexture <marioacabada@gmail.com>
2023-11-22 11:16:53 -0600

    fixed some odd stuff from the paste

--- src/draft_0.1.c
+++ src/draft_0.1.c
@@ -15,77 +15,76 @@ Welcome to GDB Online.
     
     
     
-     char ticketTypes(){
-        char ticket[4][20]= {"Economy","Business","(Premium)Economy","First class"};
-       
-       printf("The following list is of all ticket types:\n");
-       for (int i=0; i<4; i++){
-        printf("%s\n", ticket[i]);
-        }
-   }
-   char availableDestinations(){
-       printf("The following list is all available destinations:\n");
-       char destinations[4][20]= {"Maui,Hawaii","Dallas,Texas","Paris,France","Tokyo,Japan"};
-       
-       for (int i=0; i<4; i++){
-        printf("%s\n", destinations[i]);
-   }
-   }
-       
-   void arrivalDates(){
-       printf("The following list is avalible arrival dates:\n");
-       int dates[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-       
-       for (int i=0; i<20;i++){
-           printf("%d\n", dates[i]);
-       }
-   }
-   
-   
-   void departureDates(){
-   printf("The folliwng is a list of all departure dates:\n");    
-   
-       int dates[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-       
-       for (int i=0; i<20;i++){
-           printf("%d\n", dates[i]);
-       }
-
+char ticketTypes(){
+ char ticket[4][20]= {"Economy","Business","(Premium)Economy","First class"};
+ 
+ printf("The following list is of all ticket types:\n");
+ for (int i=0; i<4; i++){
+ printf("%s\n", ticket[i]);
+ }
 }
+char availableDestinations(){
+  printf("The following list is all available destinations:\n");
+  char destinations[4][20]= {"Maui,Hawaii","Dallas,Texas","Paris,France","Tokyo,Japan"};
+ 
+  for (int i=0; i<4; i++){
+  printf("%s\n", destinations[i]);
+  }
+}
+   
+void arrivalDates(){
+  printf("The following list is avalible arrival dates:\n");
+  int dates[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+  
+  for (int i=0; i<20;i++){
+    printf("%d\n", dates[i]);
+  }
+}
+
+
+void departureDates(){
+printf("The folliwng is a list of all departure dates:\n");    
+
+  int dates[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+  
+  for (int i=0; i<20;i++){
+    printf("%d\n", dates[i]);
+  }
+}
+
 int main(){
-    int destinationChoice;
-    int dateChoice;
-    int ticketChoice;
-    int numTravellers;
+int destinationChoice;
+int dateChoice;
+int ticketChoice;
+int numTravellers;
+
+printf("How many people are you travelling with?\n");
+scanf("%d", &numTravellers);
     
-    printf("How many people are you travelling with?\n");
-    scanf("%d", &numTravellers);
-        
-       while(numTravellers>10){
-           printf("Error: Group size is too large please try again\n");
-       scanf("%d", &numTravellers);
-       }
+  while(numTravellers>10){
+    printf("Error: Group size is too large please try again\n");
+  scanf("%d", &numTravellers);
+  }
     
+  //for (int i=0; i<numTravellers; i++){
+ticketTypes();
+printf("What ticket type would you like?\n");
+    //scanf("for what element they want")
     
-    //for (int i=0; i<numTravellers; i++){
-  ticketTypes();
-  printf("What ticket type would you like?\n");
-      //scanf("for what element they want")
-      
-  printf("\n");
-  availableDestinations();
-  printf("where would you like to go?\n");
-   //scanf("for what element they want")
-   
-  printf("\n");
-  arrivalDates();
-  printf("When would you like to arrive\n");
-   //scanf("for what element they want")
-   
-  printf("\n");
-  departureDates();
-  printf("When would you like to depart\n");
-    //}
-    
+printf("\n");
+availableDestinations();
+printf("where would you like to go?\n");
+ //scanf("for what element they want")
+ 
+printf("\n");
+arrivalDates();
+printf("When would you like to arrive\n");
+ //scanf("for what element they want")
+ 
+printf("\n");
+departureDates();
+printf("When would you like to depart\n");
+  //}
+  
 return 0;
 }
