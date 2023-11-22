792ece8ca2de67ccab4ae2fc0ae3a46ab09019fb
Cortexture <marioacabada@gmail.com>
2023-11-21 20:25:42 -0600

    just making sure I added the new code

--- src/hard_coded_info.c
+++ src/hard_coded_info.c
@@ -1,6 +1,10 @@
-#include <management_structs.h>
+#include "management_structs.h"
 
 int hard_coded_info(void) {
-  struct Plane planes[30];
+  struct Plane planes[10];
+  struct Airport airports[10];
+  struct Name names[20000];
+  struct DateTime date_time[66424320];
+  struct Passenger passenger[10000];
   return 0;
 }
