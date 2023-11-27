4aa572dd844efe88bd4dbd0c6c0a3080a5bef791
Cortexture <marioacabada@gmail.com>
2023-11-27 07:11:43 -0600

    figured out what i should use as my range and min, code not functional need to modify my vimrc

--- src/hard_coding/hc_planes.c
+++ src/hard_coding/hc_planes.c
@@ -2,6 +2,11 @@
 #include "../helper_functions.h"
 
 int hc_planes(void) {
-  Plane planes[50];
+  int num_plane = 50;
+  Plane planes[num_plane];
+  for (int i = 0; i < num_plane; i++) {
+    planes.range[i] = random(3400, 6000);
+    
+  }
   return 0;
 }
