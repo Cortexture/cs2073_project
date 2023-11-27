5f1179d7b5031199da855beeb111498916212a3a
Cortexture <marioacabada@gmail.com>
2023-11-27 07:42:32 -0600

    working on populating the structs some more, figured out the ranges, going to head over and do some more stuff in a bit, going to talk to girlfriend for a bit, she just dropped off the kids

--- src/hard_coding/hc_planes.c
+++ src/hard_coding/hc_planes.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <time.h>
+
 #include "../structs.h"
 #include "../helper_functions.h"
 
-int hc_planes(void) {
+int random_num(int range, int minimum) {
+  return ((int)rand() * RAND_MAX) % (range - 1) + minimum;
+}
+float frand(void) {
+  return ((double)rand() / RAND_MAX);
+}
+
+int main(void) {
   int num_plane = 50;
   Plane planes[num_plane];
+  srand(time(NULL));
   for (int i = 0; i < num_plane; i++) {
     planes[i].range = random_num(3400, 6000);
-  }
-  for (int i = 0; i < num_plane; i++) {
-    printf("%d\n", planes[i].range);
   }
   return 0;
 }
